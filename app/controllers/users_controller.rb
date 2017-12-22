626c42123a37e4da45d5ac9871cb9b569eeb3193
ikejikeji <jun.ikejima@gmail.com>
2017-12-22 09:28:56 +0000

    list13.29

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
     @user = User.find(params[:id])
-    # => app/views/users/show.html.erb
-    # debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
